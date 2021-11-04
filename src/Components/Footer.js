fae06bc3770f061b7bebab246eeae2fb7e82f731
Satyam Shree <satyamshree780@gmail.com>
2021-11-05 00:15:58 +0530

    data added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,13 +23,13 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
-                <li>
+                <li>&copy; Copyright 2021 Satyam Shree</li>
+                {/* <li>
                   Design by{" "}
                   <a title="Styleshout" href="http://www.styleshout.com/">
                     Styleshout
                   </a>
-                </li>
+                </li> */}
               </ul>
             </div>
           </Fade>
